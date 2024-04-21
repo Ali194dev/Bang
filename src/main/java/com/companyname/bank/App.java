d2cccf841a88a84ed80ac56fe3eecc4e26b3bd21
Ali194dev <114315996+Ali194dev@users.noreply.github.com>
2024-04-20 17:02:31 -0700

    changesfromgithub

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World..new change!" );
+        System.out.println( "Change code  from github repo,new change!" );
     }
 }
